b6d87fb94baa3e09ea4ff51ef44687365e2b3c9b
risahruth <187304967+risahruth@users.noreply.github.com>
2026-02-19 19:26:24 +0000

    first

--- calculator-react/src/App.jsx
+++ calculator-react/src/App.jsx
@@ -1,44 +1,50 @@
-import React from 'react'
-import {useState} from 'useState'
-const {result,setResult}=useState("");
-const handleclick=(event)={
-  setResult(result.concat(event.target.value));
-};
-const cleardisplay=()={
-  setResult("0");
-};
-const calculate=()={
-  setResult(eval(result).toString());
-};
+import React, { useState } from 'react';
+import './App.css';
 
-function app() {
-  return(
-  <>
-  <div>
-    <input type='text' placeholder='0' id='result' >${result}</input>
-    <input type='button' value='1' class='inputs' onclick={handleclick}></input>
-    <input type='button' value='2' class='inputs' onclick={handleclick}></input>
-    <input type='button' value='3' class='inputs' onclick={handleclick}></input>
-    <input type='button' value='4' class='inputs' onclick={handleclick}></input>
-    <input type='button' value='5' class='inputs' onclick={handleclick}></input>
-    <input type='button' value='6' class='inputs' onclick={handleclick}></input>
-    <input type='button' value='7' class='inputs' onclick={handleclick} ></input>
-    <input type='button' value='8' class='inputs' onclick={handleclick}></input>
-    <input type='button' value='9' class='inputs' onclick={handleclick}></input>
-    <input type='button' value='0' class='inputs' onclick={handleclick}></input>
-    <input type='button' value='+' class='inputs' onclick={handleclick}></input>
-    <input type='button' value='-' class='inputs' onclick={handleclick}></input>
-    <input type='button' value='*' class='inputs' onclick={handleclick}></input>
-    <input type='button' value='/' class='inputs' onclick={handleclick}></input>
-    <input type='button' value='.' class='inputs' onclick={handleclick}></input>
-    <input type='button' value='Clear' class='excess' onclick={cleardisplay}></input>
-    <input type='button' value='=' class='excess' onclick={calculate}></input>
-  </div>
-  </>
-  )
+function App() {
+  const [result, setResult] = useState("");
+
+  const handleClick = (event) => {
+    setResult(result + event.target.value);
+  };
+
+  const clearDisplay = () => {
+    setResult("");
+  };
+
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString());
+    } catch {
+      setResult("Error");
+    }
+  };
+
+  return (
+    <div>
+      <input type="text" id='result' value={result} readOnly />
+
+      <input type="button" value="1" class='inputs' onClick={handleClick} />
+      <input type="button" value="2" class='inputs' onClick={handleClick} />
+      <input type="button" value="3" class='inputs' onClick={handleClick} />
+      <input type="button" value="4" class='inputs' onClick={handleClick} />
+      <input type="button" value="5" class='inputs' onClick={handleClick} />
+      <input type="button" value="6" class='inputs' onClick={handleClick} />
+      <input type="button" value="7" class='inputs' onClick={handleClick} />
+      <input type="button" value="8" class='inputs' onClick={handleClick} />
+      <input type="button" value="9" class='inputs' onClick={handleClick} />
+      <input type="button" value="0" class='inputs' onClick={handleClick} />
+
+      <input type="button" value="+" class='inputs' onClick={handleClick} />
+      <input type="button" value="-" class='inputs' onClick={handleClick} />
+      <input type="button" value="*" class='inputs' onClick={handleClick} />
+      <input type="button" value="/" class='inputs' onClick={handleClick} />
+      <input type="button" value="." class='inputs' onClick={handleClick} />
+
+      <input type="button" value="Clear" class='excess' onClick={clearDisplay} />
+      <input type="button" value="=" class='excess' onClick={calculate} />
+    </div>
+  );
 }
 
-
-export default app;
-
-
+export default App;
